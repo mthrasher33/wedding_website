c96b3a6b3432218cc0807f92c6459e7c5b92e6bc
Thrasher <matt.thrasher@optum.com>
2020-03-18 16:43:50 -0500

    Custom css and images for content pages

--- src/app/content-page/content-page-routing.module.ts
+++ src/app/content-page/content-page-routing.module.ts
@@ -4,13 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContentPageComponent } from './content-page.component';
 
 const contentPageRoutes: Routes = [
-  { path: 'accommodations',  component: ContentPageComponent, data: {pageTitle: "Accommodations", heroImage: "purpleBubbles.jpg", contentKey: "accommodations"}},
+  { path: 'accommodations',  component: ContentPageComponent, data: {pageTitle: "Accommodations", heroImage: "pic4.jpg", contentKey: "accommodations"}},
   { path: 'events',  component: ContentPageComponent, data: {pageTitle: "Schedule of Events", heroImage: "purpleBubbles.jpg", contentKey: "events"}},
   { path: 'registry',  component: ContentPageComponent, data: {pageTitle: "Registry", heroImage: "purpleBubbles.jpg", contentKey: "registry"}},
-  { path: 'story',  component: ContentPageComponent, data: {pageTitle: "Our Story", heroImage: "purpleBubbles.jpg", contentKey: "story"}},
+  { path: 'story',  component: ContentPageComponent, data: {pageTitle: "Our Story", heroImage: "pic5.jpg", contentKey: "story"}},
   { path: 'wedding-party',  component: ContentPageComponent, data: {pageTitle: "Wedding Party", heroImage: "purpleBubbles.jpg", contentKey: "weddingParty"}},
-  { path: 'recommendations',  component: ContentPageComponent, data: {pageTitle: "Our Favorite Places", heroImage: "purpleBubbles.jpg", contentKey: "recommendations"}},
-  { path: 'venue',  component: ContentPageComponent, data: {pageTitle: "Venue", heroImage: "purpleBubbles.jpg", contentKey: "venue"}},
+  { path: 'recommendations',  component: ContentPageComponent, data: {pageTitle: "Our Favorite Places", heroImage: "pic3.jpg", contentKey: "recommendations"}},
+  { path: 'venue',  component: ContentPageComponent, data: {pageTitle: "Venue", heroImage: "pic2.jpg", contentKey: "venue"}},
 ];
 
 @NgModule({

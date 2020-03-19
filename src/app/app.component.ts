8d8e46ef0b6b282903e6c4d32c2fac38440f3fdc
Thrasher <matt.thrasher@optum.com>
2020-03-19 09:34:52 -0500

    Add spa google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Title }     from '@angular/platform-browser';
+import {Router, NavigationEnd} from '@angular/router'; 
 
+  // declare ga as a function to set and sent the events
+  declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -8,8 +11,17 @@ import { Title }     from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public constructor(private titleService: Title ) {
-    titleService.setTitle('Cecily and Matt');
+  public constructor(private titleService: Title, public router: Router ) {
+    titleService.setTitle('Cecily and Matt');    
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'UA-90791447-2', 
+        {
+          'page_path': event.urlAfterRedirects
+        }
+       );
+      }
+    });
    }
 
   title = 'angular-app';

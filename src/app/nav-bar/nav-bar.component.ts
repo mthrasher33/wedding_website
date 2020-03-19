1dd2032532d77baf3c4a2c51299db1be6b361d81
Thrasher <mthrash3@ms.ds.uhc.com>
2020-03-19 08:54:20 -0500

    Close mobile menu when clicking around

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
+  host: {
+    '(document:click)': 'onClick($event)',
+  },
 })
 export class NavBarComponent implements OnInit {
   hideMobileMenu : Boolean = true; 
-  constructor() { }
+  constructor(private _eref: ElementRef) { }
 
   ngOnInit(): void {
 
   }
-toggleMenu() {
-  this.hideMobileMenu = !this.hideMobileMenu;
-}
+  toggleMenu() {
+    this.hideMobileMenu = !this.hideMobileMenu;
+  }
+  onClick(event) {
+    const className : String = event.target.className;
+    console.log(className);
+    if (className !== 'mobileLink' && className.indexOf('openMobileMenu') === -1 ){ //click outside mobile menu 
+    this.hideMobileMenu = true; 
+   }
+  }
+
 }

ee003540129b80196c5283da6f49e26c0be04fd5
Thrasher <matt.thrasher@optum.com>
2020-03-18 19:01:09 -0500

    Set page name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title }     from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public constructor(private titleService: Title ) {
+    titleService.setTitle('Cecily and Matt');
+   }
+
   title = 'angular-app';
 }
